ac343c55a9aee5944e2985dfae3639c1e01a8f17
dmdjsjdj <jenia03s@gmail.com>
2025-09-09 23:22:30 +0900

    fix: 회원가입/로그인 오류 수정 (bodyParser.urlencoded 추가 및 fetch JSON 처리)

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ const __dirname = path.dirname(__filename);
 // middleware
 app.use(cors({ origin: true, credentials: true }));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); 
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 

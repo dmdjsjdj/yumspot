4894861766f625504fdbe97e8eb968517470e2ae
dmdjsjdj <jenia03s@gmail.com>
2025-09-11 05:07:11 +0900

    fix(auth): 리뷰수정

--- public/utils/supabaseClient.browser.js
+++ public/utils/supabaseClient.browser.js
@@ -1,7 +1,7 @@
-<script type="module">
-  import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
-  const SUPABASE_URL = 'https://syklxwvuubivgunkyrfa.supabase.co';
-  const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN5a2x4d3Z1dWJpdmd1bmt5cmZhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkzMDg5MDQsImV4cCI6MjA2NDg4NDkwNH0.2EL5V5QCOZcBVDuuFc5yOWTt23WfThDYWqfAeL50qxg'; // anon key는 공개 가능
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 
-  window.sb = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-</script>
+const SUPABASE_URL = 'https://syklxwvuubivgunkyrfa.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN5a2x4d3Z1dWJpdmd1bmt5cmZhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkzMDg5MDQsImV4cCI6MjA2NDg4NDkwNH0.2EL5V5QCOZcBVDuuFc5yOWTt23WfThDYWqfAeL50qxg'; // anon key는 공개 가능
+
+export const sb = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+window.sb = sb;
